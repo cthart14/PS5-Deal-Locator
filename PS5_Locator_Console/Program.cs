ca1b3faa5c195b924080adebe8629e52b56e24b3
cthart14 <christopher_hart14@outlook.com>
2025-05-26 13:06:05 -0500

    create console commit

---
+++ PS5_Locator_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        using var playwright = await Playwright.CreateAsync();
+    }
+}
